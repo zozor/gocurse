b0aa51334a2442b7e1523919a4b0651c705a6911
Anders Michelsen <kwet_z@hotmail.com>
2012-02-09 20:00:52 +0100

    first commit

--- curses.go
+++ curses.go
@@ -1,5 +1,6 @@
-package curses
+package gocurse
 
+// #cgo LDFLAGS: -lncurses
 // struct ldat{};
 // struct _win_st{};
 // #define _Bool int
@@ -8,22 +9,29 @@ package curses
 import "C"
 
 import (
-	"fmt";
-	"os";
-	"unsafe";
+	"fmt"
+	"unsafe"
+	"errors"
 )
 
-type void unsafe.Pointer;
-
-type Window C.WINDOW;
-
-type CursesError struct {
-	message string;
-}
-
-func (ce CursesError) String() string {
-	return ce.message;
-}
+var (
+	ErrorInit = errors.New("Cannot initialize")
+	ErrorWindowCreation = errors.New("Failed to create window")
+	ErrorTerminalColor = errors.New("terminal does not support color")
+	ErrorInitPair = errors.New("initializing color pair failed")
+	ErrorNoEcho = errors.New("noecho failed")
+	ErrorEcho = errors.New("echo failed")
+	ErrorSetCursor = errors.New("curs_set failed")
+	ErrorNocbreak = errors.New("Nocbreak failed")
+	ErrorCbreak = errors.New("Cbreak failed")
+	ErrorEndwin = errors.New("Endwin failed")
+	ErrorKeypad = errors.New("Keypad failed")
+	ErrorWinRefresh = errors.New("Window refresh failed")
+	ErrorWinDelete = errors.New("window deletion failed")
+	ErrorWindowMove = errors.New("window movement failed")
+	ErrorWindowSyncok = errors.New("window syncok failed")
+	ErrorNoInfo = errors.New("Something went wrong, ncurses does not know :(")
+)
 
 // Cursor options.
 const (
@@ -41,192 +49,409 @@ var ColorPairs *int = nil;
 
 var Tabsize *int = nil;
 
-// The window returned from C.initscr()
-var Stdwin *Window = nil;
-
 // Initializes gocurses
 func init() {
-	Cols = (*int)(void(&C.COLS));
-	Rows = (*int)(void(&C.LINES));
+	Cols = (*int)(unsafe.Pointer(&C.COLS));
+	Rows = (*int)(unsafe.Pointer(&C.LINES));
 	
-	Colors = (*int)(void(&C.COLORS));
-	ColorPairs = (*int)(void(&C.COLOR_PAIRS));
+	Colors = (*int)(unsafe.Pointer(&C.COLORS));
+	ColorPairs = (*int)(unsafe.Pointer(&C.COLOR_PAIRS));
 	
-	Tabsize = (*int)(void(&C.TABSIZE));
+	Tabsize = (*int)(unsafe.Pointer(&C.TABSIZE));
 }
 
-func Initscr() (*Window, os.Error) {
-	Stdwin = (*Window)(C.initscr());
-	
-	if Stdwin == nil {
-		return nil, CursesError{"Initscr failed"};
-	}
-	
-	return Stdwin, nil;
-}
-
-func Newwin(rows int, cols int, starty int, startx int) (*Window, os.Error) {
-	nw := (*Window)(C.newwin(C.int(rows), C.int(cols), C.int(starty), C.int(startx)));
-
-	if nw == nil {
-		return nil, CursesError{"Failed to create window"};
-	}
-
-	return nw, nil;
-}
-
-func (win *Window) Subwin(rows int, cols int, starty int, startx int) (*Window, os.Error)  {
-	sw := (*Window)(C.subwin((*C.WINDOW)(win), C.int(rows), C.int(cols), C.int(starty), C.int(startx)));
-
-	if sw == nil {
-		return nil, CursesError{"Failed to create window"};
-	}
-
-	return sw, nil;
-}
-
-func (win *Window) Derwin(rows int, cols int, starty int, startx int) (*Window, os.Error)  {
-	dw := (*Window)(C.derwin((*C.WINDOW)(win), C.int(rows), C.int(cols), C.int(starty), C.int(startx)));
-
-	if dw == nil {
-		return nil, CursesError{"Failed to create window"};
-	}
-
-	return dw, nil;
-}
-
-func Start_color() os.Error {
-	if int(C.has_colors()) == 0 {
-		return CursesError{"terminal does not support color"};
+/*
+====================================================
+Color Setup Functions
+====================================================
+*/
+func StartColor() error {
+	if C.has_colors() == 0 {
+		return ErrorTerminalColor
 	}
 	C.start_color();
 	
 	return nil;
 }
 
-func Init_pair(pair int, fg int, bg int) os.Error {
+func SetColorPair(pair, fg, bg int) error {
 	if C.init_pair(C.short(pair), C.short(fg), C.short(bg)) == 0 {
-		return CursesError{"Init_pair failed"};
+		return ErrorInitPair
 	}
-	return nil;
+	return nil
 }
 
-func Color_pair(pair int) int32 {
+func GetColorPair(pair int) int32 {
 	return int32(C.COLOR_PAIR(C.int(pair)));
 }
 
-func Noecho() os.Error {
+/*
+====================================================
+Curses Setup Functions
+====================================================
+*/
+
+func Noecho() error {
 	if int(C.noecho()) == 0 {
-		return CursesError{"Noecho failed"};
+		return ErrorNoEcho
 	}
 	return nil;
 }
 
-func Echo() os.Error {
-	if int(C.noecho()) == 0 {
-		return CursesError{"Echo failed"};
+func Echo() error {
+	if int(C.echo()) == 0 {
+		return ErrorEcho
 	}
 	return nil;
 }
 
-func Curs_set(c int) os.Error {
-	if C.curs_set(C.int(c)) == 0 {
-		return CursesError{"Curs_set failed"};
+func SetCursor(c int) error {
+	if C.curs_set(C.int(c)) == ERR {
+		return ErrorSetCursor
+	}
+	return nil
+}
+
+
+func Nocbreak() error {
+	if C.nocbreak() == ERR {
+		return ErrorNocbreak
 	}
 	return nil;
 }
 
-func Nocbreak() os.Error {
-	if C.nocbreak() == 0 {
-		return CursesError{"Nocbreak failed"};
+
+func Cbreak() error {
+	if C.cbreak() == ERR {
+		return ErrorCbreak
 	}
 	return nil;
 }
 
-func Cbreak() os.Error {
-	if C.cbreak() == 0 {
-		return CursesError{"Cbreak failed"};
+func Getch() int {
+	return int(C.getch())
+}
+
+
+func Endwin() error {
+	if C.endwin() == ERR {
+		return ErrorEndwin
 	}
 	return nil;
 }
 
-func Endwin() os.Error {
-	if C.endwin() == 0 {
-		return CursesError{"Endwin failed"};
+func Raw() error {
+	if C.raw() == ERR {
+		return ErrorNoInfo //Forkert ERROR
 	}
 	return nil;
 }
 
-func (win *Window) Getch() int {
-	return int(C.wgetch((*C.WINDOW)(win)));
+func Halfdelay(delay int) error {
+	if C.halfdelay(C.int(delay)) == ERR {
+		return ErrorNoInfo //Forkert ERROR
+	}
+	return nil
 }
 
-func (win *Window) Addch(x, y int, c int32, flags int32) {
-	C.mvwaddch((*C.WINDOW)(win), C.int(y), C.int(x), C.chtype(c) | C.chtype(flags));
+/*
+====================================================
+Type Window
+====================================================
+*/
+
+type Window struct {
+	win *C.WINDOW
+}
+
+func Initscr() (*Window, error) {
+	win := C.initscr()
+
+	if win == nil {
+		return nil, ErrorInit
+	}
+	
+	return &Window{win}, nil
+}
+
+func NewWindow(rows, cols, starty, startx int) (*Window, error) {
+	nw := C.newwin(C.int(rows), C.int(cols), C.int(starty), C.int(startx))
+
+	if nw == nil {
+		return nil, ErrorWindowCreation
+	}
+
+	return &Window{nw}, nil;
+}
+
+func (w *Window) Delwindow() error {
+	if C.delwin(w.win) == ERR {
+		return ErrorWinDelete
+	}
+	return nil
+}
+
+func (w *Window) Getch() int {
+	return int(C.wgetch(w.win))
+}
+
+func (w *Window) Getstr() (string, error) {
+	var buffer [1024]C.char
+	if C.wgetnstr(w.win, &buffer[0], 1024) == ERR {
+		return "", ErrorNoInfo
+	}
+	
+	s := C.GoString(&buffer[0])
+	return s, nil
+}
+
+func (w *Window) Timeout(delay int) {
+	C.wtimeout(w.win, C.int(delay))
+}
+
+func (w *Window) Nodelay(nodelay bool) {
+	if nodelay {
+		C.nodelay(w.win, 1)
+	} else {
+		C.nodelay(w.win, 0)
+	}
+}
+
+func (w *Window) Addch(x, y int, c int32, flags int32) {
+	C.mvwaddch(w.win, C.int(y), C.int(x), C.chtype(c) | C.chtype(flags));
 }
 
 // Since CGO currently can't handle varg C functions we'll mimic the
 // ncurses addstr functions.
-func (win *Window) Addstr(x, y int, str string, flags int32, v ...interface{}) {
-	newstr := fmt.Sprintf(str, v);
+func (w *Window) Addstr(x, y int, str string, flags int32, v ...interface{}) {
+	newstr := fmt.Sprintf(str, v...);
 	
-	win.Move(x, y);
+	w.MoveCursor(x, y);
 	
 	for i := 0; i < len(newstr); i++ {
-		C.waddch((*C.WINDOW)(win), C.chtype(newstr[i]) | C.chtype(flags));
+		C.waddch(w.win, C.chtype(newstr[i]) | C.chtype(flags));
 	}
+}
+
+func (w *Window) Border(ls, rs, ts, bs, tl, tr, bl, br int) error {
+	if C.wborder(w.win, C.chtype(ls), C.chtype(rs),
+	 C.chtype(ts), C.chtype(bs), C.chtype(tl), C.chtype(tr),
+          C.chtype(bl), C.chtype(br)) == ERR {
+          
+          return ErrorNoInfo
+    }
+    return nil
 }
 
 // Normally Y is the first parameter passed in curses.
-func (win *Window) Move(x, y int) {
-	C.wmove((*C.WINDOW)(win), C.int(y), C.int(x));
+func (w *Window) MoveCursor(x, y int) error {
+	if C.wmove(w.win, C.int(y), C.int(x)) == ERR {
+		return ErrorWindowMove
+	}
+	return nil
 }
 
-func (w *Window) Keypad(tf bool) os.Error {
+func DublicateWindow(w *Window) *Window {
+	return &Window{C.dupwin(w.win)}
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) MoveDerwindow(x,y int) error {
+	if C.mvderwin(w.win, C.int(y), C.int(x)) == ERR {
+		return ErrorWindowMove
+	}
+	return nil
+}
+
+
+
+//Flakey function according to ncurses DOC
+func (w *Window) Wsyncup() {
+	C.wsyncup(w.win)
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) Syncok(bf bool) error {
+	var bfint int
+	if bf {
+		bfint++
+	}
+	
+	if C.syncok(w.win, C.int(bfint)) == ERR {
+		return ErrorWindowSyncok
+	}
+	return nil
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) Wcursyncup() {
+	C.wcursyncup(w.win)
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) Wsyncdown() {
+	C.wsyncdown(w.win)
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) SubWindow(rows, cols, starty, startx int) (*Window, error)  {
+	sw := C.subwin(w.win, C.int(rows), C.int(cols), C.int(starty), C.int(startx))
+
+	if sw == nil {
+		return nil, ErrorWindowCreation
+	}
+
+	return &Window{sw}, nil
+}
+
+//Flakey function according to ncurses DOC
+func (w *Window) Derwin(rows, cols, starty, startx int) (*Window, error)  {
+	dw := C.derwin(w.win, C.int(rows), C.int(cols), C.int(starty), C.int(startx))
+
+	if dw == nil {
+		return nil, ErrorWindowCreation
+	}
+
+	return &Window{dw}, nil;
+}
+
+func (w *Window) Keypad(tf bool) error {
 	var outint int;
-	if tf == true {outint = 1;}
-	if tf == false {outint = 0;}
-	if C.keypad((*C.WINDOW)(w), C.int(outint)) == 0 {
-		return CursesError{"Keypad failed"};
+	if tf {
+		outint++
 	}
-	return nil;
-}
-
-func (win *Window) Refresh() os.Error {
-	if C.wrefresh((*C.WINDOW)(win)) == 0 {
-		return CursesError{"refresh failed"};
+	if C.keypad(w.win, C.int(outint)) != OK {
+		return ErrorKeypad
 	}
-	return nil;
+	return nil
 }
 
-func (win *Window) Redrawln(beg_line, num_lines int) {
-	C.wredrawln((*C.WINDOW)(win), C.int(beg_line), C.int(num_lines));
+func (w *Window) Refresh() error {
+	if C.wrefresh(w.win) == ERR {
+		return ErrorWinRefresh
+	}
+	return nil
 }
 
-func (win *Window) Redraw() {
-	C.redrawwin((*C.WINDOW)(win));
+func (w *Window) Redrawln(beg_line, num_lines int) {
+	C.wredrawln(w.win, C.int(beg_line), C.int(num_lines));
 }
 
-func (win *Window) Clear() {
-	C.wclear((*C.WINDOW)(win));
+func (w *Window) Redraw() {
+	C.redrawwin(w.win);
 }
 
-func (win *Window) Erase() {
-	C.werase((*C.WINDOW)(win));
+func (w *Window) Clear() {
+	C.wclear(w.win);
 }
 
-func (win *Window) Clrtobot() {
-	C.wclrtobot((*C.WINDOW)(win));
+func (w *Window) Erase() {
+	C.werase(w.win);
 }
 
-func (win *Window) Clrtoeol() {
-	C.wclrtoeol((*C.WINDOW)(win));
+func (w *Window) Clrtobot() {
+	C.wclrtobot(w.win);
 }
 
-func (win *Window) Box(verch, horch int) {
-	C.box((*C.WINDOW)(win), C.chtype(verch), C.chtype(horch));
+func (w *Window) Clrtoeol() {
+	C.wclrtoeol(w.win);
 }
 
-func (win *Window) Background(colour int32) {
-	C.wbkgd((*C.WINDOW)(win), C.chtype(colour));
+func (w *Window) Box(verch, horch int) {
+	C.box(w.win, C.chtype(verch), C.chtype(horch));
 }
+
+func (w *Window) Background(colour int32) {
+	C.wbkgd(w.win, C.chtype(colour));
+}
+
+//Attributes
+func (w *Window) Attroff(attrs int) error {
+	if C.wattroff(w.win, C.int(attrs)) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Attron(attrs int) error {
+	if C.wattron(w.win, C.int(attrs)) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Attrset(attrs int) error {
+	if C.wattrset(w.win, C.int(attrs)) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Color_set(pair int) error {
+	if C.wcolor_set(w.win, C.short(pair), nil) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Standend() error {
+	if C.wstandend(w.win) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Standout() error {
+	if C.wstandout(w.win) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) Attr_get() (int, int, error) {
+	var attr C.attr_t
+	var pair C.short
+	
+	if C.wattr_get(w.win, &attr, &pair, nil) == ERR {
+		return 0,0, ErrorNoInfo
+	}
+	
+	return int(attr), int(pair), nil
+}
+
+func (w *Window) Chgat(n, attr, color int) error {
+	if C.wchgat(w.win, C.int(n), C.attr_t(attr), C.short(color), nil) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+func (w *Window) MoveChgat(x, y, n, attr, color int) error {
+	if C.mvwchgat(w.win, C.int(y), C.int(x), C.int(n), C.attr_t(attr), C.short(color), nil) == ERR {
+		return ErrorNoInfo
+	}
+	return nil
+}
+
+//Window sizes - Undefined for some reason?
+/*
+func (w *Window) Getyx() (x int, y int) {
+	C.getyx(w.win, &y, &x)
+	return
+}
+
+func (w *Window) Getparyx() (x int, y int) {
+	C.getparyx(w.win, &y, &x)
+	return
+}
+
+func (w *Window) Getbegyx() (x int, y int) {
+	C.getbegyx(w.win, &y, &x)
+	return
+}
+
+func (w *Window) Getmaxyx() (x int, y int) {
+	C.getmaxyx(w.win, &y, &x)
+	return
+}
+*/
